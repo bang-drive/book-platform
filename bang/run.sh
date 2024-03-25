4c784952e700394c2dbb4491d5097bd0fca07751
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2024-03-24 21:10:57 -0400

    Set kart list.

--- bang/run.sh
+++ bang/run.sh
@@ -8,13 +8,19 @@ if [ ! -f "${BIN}" ]; then
     ./bang/build.sh
 fi
 
+# Avoid karts like gnu, sara_the_racer, xue.
+ALLOWED_KARTS=(adiumy amanda emule beastie gavroche hexley kiki konqi nolok pidgin puffy sara_the_wizard suzanne tux wilber)
+shuffled_karts=($(shuf -e "${ALLOWED_KARTS[@]}"))
+num_karts=${#ALLOWED_KARTS[@]}
+
 # https://manpages.debian.org/testing/supertuxkart/supertuxkart.1
 # scotland: Nessie's Pond
 # snowmountain: Northern Resort
 "${BIN}" \
     --laps=5 \
     --no-start-screen \
-    --numkarts=16 \
+    --numkarts=$((num_karts + 1)) \
+    --ai=$(IFS=,; echo "${shuffled_karts[*]}") \
     --race-now \
     --screensize=1920x1080 \
     --track="scotland"
